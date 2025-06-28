7f281a7bc595366315c84947137c1a63caffbf36
adufou <antoine.du.fou@gmail.com>
2025-06-29 00:19:08 +0200

    chore: add bases as defined in our doc (without implicit yet)

---
+++ src/models/items/item_base/item_base.gd
@@ -0,0 +1,7 @@
+class_name ItemBase
+extends Resource
+
+@export var type: ItemType.ItemType
+@export var name: String
+@export var texture: Texture2D
+@export var id: String
--- src/models/items/item_blueprint/stats/pools/item_stats_pools.gd
+++
@@ -1,9 +0,0 @@
-class_name ItemStatsPools
-extends Resource
-
-@export var boots: ItemRarityStatsPools
-@export var chestplates: ItemRarityStatsPools
-@export var gloves: ItemRarityStatsPools
-@export var helmets: ItemRarityStatsPools
-@export var shields: ItemRarityStatsPools
-@export var swords: ItemRarityStatsPools

a5e888c999df43f16506c787eedfef05742d2144
adufou <antoine.du.fou@gmail.com>
2025-06-29 00:46:07 +0200

    chore: add item bases

---
+++ src/models/items/item_base/item_bases.gd
@@ -0,0 +1,9 @@
+class_name ItemBases
+extends Resource
+
+@export var boots: Array[ItemBase]
+@export var chestplates: Array[ItemBase]
+@export var gloves: Array[ItemBase]
+@export var helmets: Array[ItemBase]
+@export var shields: Array[ItemBase]
+@export var swords: Array[ItemBase]

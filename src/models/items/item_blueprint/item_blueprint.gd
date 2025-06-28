824481eb72fe0a49276e079ea7d99103bc3114bb
adufou <antoine.du.fou@gmail.com>
2025-06-29 00:19:08 +0200

    chore: add bases as defined in our doc (without implicit yet)

--- src/models/items/item_blueprint/item_blueprint.gd
+++
@@ -1,8 +0,0 @@
-class_name ItemBlueprint
-extends Resource
-
-@export var rarity: ItemRarity.ItemRarity
-@export var type: ItemType.ItemType
-@export var name: String
-@export var texture: Texture2D
-@export var id: String

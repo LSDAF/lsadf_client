6dd798d4b8d756e97604ea4555502bbd8ca10a1b
adufou <antoine.du.fou@gmail.com>
2025-06-28 21:55:43 +0200

    feat: separate concerns and implement implicit as affix

--- src/models/items/item_blueprint/item_blueprint.gd
+++ src/models/items/item_blueprint/item_blueprint.gd
@@ -2,7 +2,6 @@ class_name ItemBlueprint
 extends Resource
 
 @export var rarity: ItemRarity.ItemRarity
-@export_range(1, 100, 1) var level: int
 @export var type: ItemType.ItemType
 @export var name: String
 @export var texture: Texture2D

3fc5855fa4e437e42d0f4b86fa53bac287d92e32
Louis SANTUCCI <louissantucci1998@gmail.com>
2025-09-04 19:04:42 +0200

    chore: renamed parameter in  to improve clarity

--- src/models/http/inventory/fetch_inventory_dto.gd
+++ src/models/http/inventory/fetch_inventory_dto.gd
@@ -3,6 +3,6 @@ class_name FetchInventoryDto
 var items: Array[InventoryItemDto]
 
 
-func _init(items: Array) -> void:
-	for item: Dictionary in items:
+func _init(item_array: Array) -> void:
+	for item: Dictionary in item_array:
 		items.push_back(InventoryItemDto.new(item))

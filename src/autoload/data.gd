63f35271350054be5b9ab70bb51f0e7167710d70
Louis SANTUCCI <louissantucci1998@gmail.com>
2025-09-26 18:26:42 +0200

    feat(game_session): add game session management functionality
    * Update API routes to include endpoints for opening and refreshing game sessions.
    * Load game session data in the data module.
    * Initialize game session service in the services module.
    * Added game sesion API in api autoload

--- src/autoload/data.gd
+++ src/autoload/data.gd
@@ -10,6 +10,9 @@ var current_quest: CurrentQuestData = (
 )
 var difficulty: DifficultyData = preload("res://src/data/difficulty/difficulty_data.gd").new()
 var game_save: GameSaveData = preload("res://src/data/game_save/game_save_data.gd").new()
+var game_session: GameSessionData = (
+	preload("res://src/data/game_session/game_session_data.gd").new()
+)
 var inventory: InventoryData = preload("res://src/data/inventory/inventory_data.gd").new()
 var user_local_data: UserLocalData = preload("res://src/data/user_local/user_local_data.gd").new()
 var stage: StageData = preload("res://src/data/stage/stage_data.gd").new()

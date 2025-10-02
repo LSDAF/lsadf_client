aa161af7d5e3b4c5e6fa1ed1e4d67555408385d8
Louis SANTUCCI <louissantucci1998@gmail.com>
2025-10-02 17:24:43 +0200

    feat(inventory): refactor item management and organization
    * Simplified item update logic by introducing dedicated functions for fetching items to update, insert, and delete.
    * Improved thread safety with mutexes for item queues.
    * Enhanced organization of items for inventory operations.

--- src/services/inventory/inventory_service.gd
+++ src/services/inventory/inventory_service.gd
@@ -1,15 +1,58 @@
 class_name InventoryService
 
 var _inventory_data: InventoryData
+var _insert_item_queue: Queue
+var _update_item_queue: Queue
+var _delete_item_queue: Queue
+
+# Mutexes for thread safety
+var _insert_mutex: Mutex
+var _update_mutex: Mutex
+var _delete_mutex: Mutex
+
 var _item_pools: ItemPools = preload("res://src/resources/items/item_pools/item_pools.tres")
 
 
 func _init(inventory_data: InventoryData) -> void:
 	_inventory_data = inventory_data
+	_insert_item_queue = Queue.new()
+	_update_item_queue = Queue.new()
+	_delete_item_queue = Queue.new()
+	_insert_mutex = Mutex.new()
+	_update_mutex = Mutex.new()
+	_delete_mutex = Mutex.new()
+
+
+func get_items_to_update() -> Array[Item]:
+	_update_mutex.lock()
+	var items_to_update: Array[Item] = []
+	while not _update_item_queue.is_empty():
+		items_to_update.push_back(_update_item_queue.dequeue())
+	_update_mutex.unlock()
+	return items_to_update
+
+
+func get_items_to_insert() -> Array[Item]:
+	_insert_mutex.lock()
+	var items_to_insert: Array[Item] = []
+	while not _insert_item_queue.is_empty():
+		items_to_insert.push_back(_insert_item_queue.dequeue())
+	_insert_mutex.unlock()
+	return items_to_insert
+
+
+func get_items_to_delete() -> Array[Item]:
+	_delete_mutex.lock()
+	var items_to_delete: Array[Item] = []
+	while not _delete_item_queue.is_empty():
+		items_to_delete.push_back(_delete_item_queue.dequeue())
+	_delete_mutex.unlock()
+	return items_to_delete
 
 
 func add_item(item: Item) -> void:
 	_inventory_data.items.push_back(item)
+	_insert_item_queue.enqueue(item)
 
 	EventBus.inventory_update.emit()
 
@@ -44,6 +87,7 @@ func equip_item(item_client_id: String) -> void:
 			continue
 
 		items[index].is_equipped = items[index].client_id == item_client_id
+		_update_item_queue.enqueue(items[index])
 
 	EventBus.inventory_update.emit()
 

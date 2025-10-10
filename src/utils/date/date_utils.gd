79c59aad0c546bebf9bfedfd1d270da049db441a
Louis SANTUCCI <louissantucci1998@gmail.com>
2025-10-10 14:03:44 +0200

    feat(date_utils): add utility to add seconds to datetime - Implemented `add_seconds_to_time` function to add seconds to a datetime dictionary. - Added validation for datetime dictionary structure. - Included unit tests for various scenarios including positive, negative, and rollover cases.

--- src/utils/date/date_utils.gd
+++ src/utils/date/date_utils.gd
@@ -22,6 +22,39 @@ static func format_unix_time_to_date_string(unix_time: int) -> String:
 	return date
 
 
+## Adds seconds to a datetime dictionary and returns a new datetime dictionary
+static func add_seconds_to_time(datetime: Dictionary, seconds: int) -> Dictionary:
+	if !validate_datetime_dict(datetime):
+		return {}
+	var new_time := Time.get_unix_time_from_datetime_dict(datetime)
+	if new_time == null:
+		push_error("Failed to convert date dictionary to unix time: {0}".format([str(datetime)]))
+		return {}
+	new_time += seconds
+	var new_date := Time.get_datetime_dict_from_unix_time(new_time)
+	if new_date == null:
+		push_error(
+			"Failed to convert unix time back to date dictionary: {0}".format([str(new_time)])
+		)
+		return {}
+	return new_date
+
+
+static func validate_datetime_dict(date: Dictionary) -> bool:
+	var required_keys := ["year", "month", "day", "hour", "minute", "second"]
+	for key: String in required_keys:
+		if not date.has(key):
+			push_error("Date dictionary is missing key: {0}".format([key]))
+			return false
+		if typeof(date[key]) != TYPE_INT:
+			push_error("Date dictionary key {0} is not an integer".format([key]))
+			return false
+		if date[key] < 0:
+			push_error("Date dictionary key {0} is not a positive integer".format([key]))
+			return false
+	return true
+
+
 ## Compares two datetime dictionaries and returns:
 ## -1 if date1 is before date2
 ##  0 if date1 is equal to date2
